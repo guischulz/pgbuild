44d1519c7bb2a12eec200296d7cfc6ed4aa4a5ae
Guido Schulz <guischulz@users.noreply.github.com>
2021-10-09 18:47:02 +0200

    Build with PostgreSQL 14

--- environment.bat
+++ environment.bat
@@ -2,12 +2,12 @@ rem Visual Studio 2017 C++ build tools
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\WDExpress\VC\Auxiliary\Build\vcvarsall.bat" x86_x64
 
 set GIT=D:\work\git-2.8.1
-set CMAKE=D:\work\cmake-3.19.7-win64-x64
+set CMAKE=D:\work\cmake-3.21.3-windows-x86_64
 set HHC=C:\Tools\HTML Help Workshop
 set PYTHON=C:\Python36
 
 set PGBUILD=%~dp0
-set PGDIR=C:\Work\postgresql-13.4-1
+set PGDIR=D:\Work\postgresql-14.0-1
 set WXWIN=D:\work\wxWidgets-3.1.3
 
 rem https://github.com/guischulz/pgadmin3
